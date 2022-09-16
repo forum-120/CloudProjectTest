dd073a1179d92ee3de016d40fb6f85b8cb6ceca5
rich chan <1120969294@qq.com>
2022-09-17 00:02:24 +0800

    add demo

---
+++ 1的副本.js
@@ -0,0 +1,8 @@
+let http = require('http');
+
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.end('HEllo wirld\n');
+}).listen(8888);
+
+console.log('Server running at http://127.0.0.1:8888/');
